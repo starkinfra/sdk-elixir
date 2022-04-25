dbf24aef10ac5759748caf1eeca4375d8a453ae2
Adryan Alencar <adryan.alencar@starkbank.com>
2022-04-25 14:15:26 -0300

    initial structure for sdk

---
+++ config/config.exs
@@ -0,0 +1,9 @@
+import Config
+
+config :starkinfra,
+  language: "en-US",
+  project: [
+    environment: :sandbox,
+    id: System.fetch_env!("DEVELOPMENT_ID"), # "9999999999999999",
+    private_key: System.fetch_env!("DEVELOPMENT_PRIVATE_KEY") # "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIBEcEJZLk/DyuXVsEjz0w4vrE7plPXhQxODvcG1Jc0WToAcGBSuBBAAK\noUQDQgAE6t4OGx1XYktOzH/7HV6FBukxq0Xs2As6oeN6re1Ttso2fwrh5BJXDq75\nmSYHeclthCRgU8zl6H1lFQ4BKZ5RCQ==\n-----END EC PRIVATE KEY-----"
+  ]
---
+++ mix.exs
@@ -0,0 +1,47 @@
+defmodule StarkInfra.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :starkinfra,
+      name: :starkinfra,
+      version: "2.6.2",
+      homepage_url: "https://starkinfra.com",
+      source_url: "https://github.com/starkinfra/sdk-elixir",
+      description: description(),
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Stark Infra"],
+      licenses: [:MIT],
+      links: %{
+        "StarkInfra" => "https://starkinfra.com",
+        "GitHub" => "https://github.com/starkinfra/sdk-elixir"
+      }
+    ]
+  end
+
+  defp description do
+    "SDK to make integrations with the Stark Infra API easier."
+  end
+
+  def application do
+    [
+      extra_applications: [:inets]
+    ]
+  end
+
+  defp deps do
+    [
+      {:starkbank_ecdsa, "~> 1.1.0"},
+      {:jason, "~> 1.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+end
